3cc13ddb93a582957e00a94cb6cc46e7a257970f
DEVESHDS <dev1710.sharma@gmail.com>
2021-04-27 19:38:45 +0530

    chat footer

---
+++ src/Chat.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
+
+import "./Chat.css";
+
+function Chat() {
+  const [input, setInput] = useState("");
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("send message is called ", input);
+    setInput("");
+  };
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at ....</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Ashish singh</span>Hey Guys
+          <span className="chat__timestamp"> 3:52PM</span>
+        </p>
+        <p className="chat__message">Hey Guys</p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send Message
+          </button>
+        </form>
+        <Mic />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
